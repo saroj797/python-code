8525d0de0dfaa652720c5fac358690068650e1b6
saroj797 <sarojraaz234@gmail.com>
2025-08-28 10:43:35 +0530

    yes

---
+++ simplesum.py
@@ -0,0 +1,11 @@
+# Program to find sum of two numbers
+
+# Taking input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Calculating sum
+sum = num1 + num2
+
+# Displaying result
+print("The sum of", num1, "and", num2, "is:", sum)
