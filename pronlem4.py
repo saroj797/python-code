989d2c835944ad7168c4ea4b1db6f99f27918d3c
saroj797 <sarojraaz234@gmail.com>
2025-08-29 19:39:14 +0530

    yes

---
+++ pronlem4.py
@@ -0,0 +1,10 @@
+a = int(input("enter the valua of a ="))
+print(type(a))
+
+
+b = int(input("Enter the num1 = "))
+c = int(input("enter the num2= "))
+
+print("b is greater than c ", b>c) 
+
+print("the square of a number a= ", a*a)
