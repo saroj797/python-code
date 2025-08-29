dfc2801a8c5afd4c43ce88b062b30b450e9f3dfa
saroj797 <sarojraaz234@gmail.com>
2025-08-29 19:06:17 +0530

    yes

---
+++ variables2.py
@@ -0,0 +1,8 @@
+a =1 # a is a integer
+
+b = 2.3 # b is a float
+
+c = "saroj" # c is string
+print("a")
+ 
+print("b")
---
+++ variables3.py
@@ -0,0 +1,21 @@
+# Arithmatic operator
+
+a = 5
+b = 8
+
+c = a + b
+
+print(c)
+
+#Assignment operator
+
+a = 4-5 #assign 4-5 in a
+
+print(a)
+
+b = 3
+b -= 5
+
+print(b)
+
+ 
