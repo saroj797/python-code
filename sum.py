2bd91b3f8697bddf98d01961f2c88a092bcf6240
saroj797 <sarojraaz234@gmail.com>
2025-08-29 19:15:37 +0530

    yes

---
+++ sum.py
@@ -0,0 +1,6 @@
+# WAp a program t print sum of two number
+
+a =int(input("enter the num1 = "))    # int for converting string data type to integer data type
+b  = int(input("enter the num2 = "))    # int for converting string data type to integer data type
+
+print("print the sum", a + b)
