960a447a0d3c54faf0ffaced524110ac414c9ef2
saroj797 <sarojraaz234@gmail.com>
2025-08-31 22:13:19 +0530

    yes

---
+++ touples.py
@@ -0,0 +1,9 @@
+a = (1, 23, 342,23, False, "saroj", "Aman",2.3)
+
+print(a)
+
+print(type(a)) #  check the a  which  type of data
+
+print(a.count(23)) # this function will returns the number of times 23 returns occur in a
+
+print(a.index(23))  #this function rturns the of first occurance of 23 in a
