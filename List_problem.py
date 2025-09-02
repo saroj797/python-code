cc72c9be594a70596b7a75bbe5618ca07ee38f59
saroj797 <sarojraaz234@gmail.com>
2025-09-02 18:15:27 +0530

    yes

---
+++ List_problem.py
@@ -0,0 +1,25 @@
+# Basic concept
+
+number = [1, 3, 4, 5, 345,"saroj",bool, 566,]
+
+print(number[5])
+
+number[5] = "kumar" # so list is mutable
+
+print(number[5])
+print(number[0:6]) # so list list is also slicing
+
+number.append("Bos")  # append function is add the new data at the end 
+
+print(number)
+
+number1 = [1,2,343,345,45,56,4,] #list of number
+
+number1.sort() # this function is design the list in ascending order
+print(number1)
+
+number1.reverse() # this function reverse the list
+print(number1)
+
+number1.remove(4)
+print(number1)
