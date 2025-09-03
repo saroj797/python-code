fe6b86c39b4e3e1ffbc51a68eeddc7c549c231a5
saroj797 <sarojraaz234@gmail.com>
2025-09-03 19:20:11 +0530

    yes

--- List_problem.py
+++ List_problem.py
@@ -23,3 +23,35 @@ print(number1)
 
 number1.remove(4)
 print(number1)
+
+
+
+
+#Q1. e a program to store a seven fruits in a list inter by suer
+
+fruits = []
+
+f1 = input("Enter the fruits name1= ")
+fruits.append(f1)
+
+f2= input("Enter the fruits name2= ")
+fruits.append(f2)
+
+f3= input("Enter the fruits name3= ")
+fruits.append(f3)
+
+f4= input("Enter the fruits name4= ")
+fruits.append(f4)
+
+f5= input("Enter the fruits name5= ")
+fruits.append(f5)
+
+f6= input("Enter the fruits name6= ")
+fruits.append(f6)
+
+f7= input("Enter the fruits name7= ")
+fruits.append(f7)
+
+print(fruits)
+
+
---
+++ marks.py
@@ -0,0 +1,26 @@
+
+Marks = []
+
+m1 = int(input("Enter   the marks of student 1 = "))
+Marks.append(m1)
+
+m2 = int(input(" Enter the marks of student 2 = "))
+Marks.append(m2)
+
+m3=int(input(" Enter the marks of student 3= "))
+Marks.append(m3)
+
+m4 =  int( input("nter e marks of student 4= "))
+Marks.append(m4)
+
+m5=  int( input(" Enter the marks of student 5= "))
+Marks.append(m5)
+
+m6=  int(input(" Enter the marks of student 6= "))
+Marks.append(m6)
+
+print(Marks)
+
+Marks.sort()
+print("show after the sorting Marks",Marks)
+
